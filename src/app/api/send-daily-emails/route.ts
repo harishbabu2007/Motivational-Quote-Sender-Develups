7becd80692300f787bc57160260bb71fb72eaf21
harish babu <harishbabubalaji@gmail.com>
2025-08-31 13:53:43 +0530

    Backend: Tried to fix some issues

--- src/app/api/send-daily-emails/route.ts
+++ src/app/api/send-daily-emails/route.ts
@@ -21,16 +21,21 @@ export async function GET() {
     const client = await clientPromise;
     const db = client.db("motivation-app");
 
-    const users = await db.collection("users").find().toArray();
+    // const users = await db.collection("users").find().toArray();
 
-    for (const user of users) {
-      const quote = await get_random_quote();
+    const users = await db
+      .collection("users")
+      .find({}, { readConcern: { level: "majority" } })
+      .toArray();
 
-      await transporter.sendMail({
-        from: `"Motivation Quotes" <${process.env.EMAIL_USER}>`,
-        to: user.email,
-        subject: "Your Daily Motivation... :) | FROM DEVELUPS",
-        html: `
+      await Promise.allSettled(
+        users.map(async (user) => {
+          const quote = await get_random_quote();
+          await transporter.sendMail({
+            from: `"Motivation Quotes" <${process.env.EMAIL_USER}>`,
+            to: user.email,
+            subject: "Your Daily Motivation... :) | FROM DEVELUPS",
+            html: `
             <div style="padding:10px;">
             <h2>Good Morning! Here's a nice quote to get ur day started....</h2>
             <p>${quote}</p>
@@ -46,8 +51,36 @@ export async function GET() {
             </p>
             </div>
         `,
-      });
-    }
+          });
+        })
+      );
+
+
+    // for (const user of users) {
+    //   const quote = await get_random_quote();
+
+    //   await transporter.sendMail({
+    //     from: `"Motivation Quotes" <${process.env.EMAIL_USER}>`,
+    //     to: user.email,
+    //     subject: "Your Daily Motivation... :) | FROM DEVELUPS",
+    //     html: `
+    //         <div style="padding:10px;">
+    //         <h2>Good Morning! Here's a nice quote to get ur day started....</h2>
+    //         <p>${quote}</p>
+    //         <hr />
+    //         <p style="font-size:12px; color:gray;">
+    //             If you no longer want these emails, 
+    //             <a href="https://your-app.com/api/unsubscribe?email=${encodeURIComponent(
+    //               user.email
+    //             )}">
+    //             Unsubscribe
+    //             </a>
+
+    //         </p>
+    //         </div>
+    //     `,
+    //   });
+    // }
 
     return NextResponse.json({ success: true, sent: users.length });
   } catch (err: any) {
